b324bd2f672831bad28973aea12f408d9638a29f
AiviriX <claveriakyle@gmail.com>
2022-05-15 20:34:38 +0800

    [Update] Package

--- MP2/Lexer/Lexer.java
+++ MP2/Lexer/Lexer.java
@@ -1,14 +1,20 @@
 package MP2.Lexer;
 
 public class Lexer {
-    
+    private String fn;
+    private String text;
+
     
     Lexer(String fn, String text){
+        this.fn = fn;
+        this.text = text;
         
+        //this.current_char = none;
+        this.advance();
     }    
 
     public void advance(){
-        
+
     }
 
     public void make_tokens(){
