487173a727c3934b71b6a35de6ee83d2db44aa04
Ella Fritz Sarmiento <sellafritz@gmail.com>
2022-05-15 13:42:00 +0800

    [Update] Little Changes

--- MP2/Lexer/Lexer.java
+++ MP2/Lexer/Lexer.java
@@ -2,4 +2,20 @@ package MP2;
 
 public class Lexer {
     
+    
+    Lexer(String fn, String text){
+        
+    }    
+
+    public void advance(){
+        
+    }
+
+    public void make_tokens(){
+
+    }
+
+    public void make_numbers(){
+        
+    }
 }
--- MP2/Lexer/Position.java
+++ MP2/Lexer/Position.java
@@ -1,4 +1,15 @@
 package MP2;
 public class Position {
     
+    Position(){
+
+    }
+
+    public void advance(){
+
+    }   
+    
+    public void copy(){
+        
+    }
 }
