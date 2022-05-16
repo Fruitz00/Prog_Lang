027a671731c0a4b8466d346f10a026cc59fc6016
AiviriX <claveriakyle@gmail.com>
2022-05-16 23:57:06 +0800

    [Fix] Everything

--- MP2/Lexer/Tokens.java
+++ MP2/Lexer/Tokens.java
@@ -1,26 +1,30 @@
+package MP2.Lexer;
 import java.text.Normalizer.Form;
 
-public class Tokens{
-    private String token;
-    private String value;
+public class Tokens {
+    static final int[] DIGITS = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+    static final String TT_INT = "INT";
+    static final String TT_FLOAT = "FLOAT";
+    static final char TT_PLUS = '+';
+    static final char TT_MINUS = '-';
+    static final String TT_DIV = "DIV";
+    static final String TT_LPAREN = "LPAREN";
+    static final String TT_RPAREN = "RPAREN";
 
+    private String type_;
+    private int value;
 
-    public token(String token, String value){
-
-        this.token = token;
-        this. value = value;
-
-    Tokens(char type_, int value = null){
-        this.type = type_;
-        this.value = value;
+    public Tokens(String type_, int value){
+            this.type_ = type_;
+            this.value = value;
     }
 
 
-    public toStr(){
-
-        if (this.value){
-            return String.format("%s", String.valueOf(this.token) + String.valueOf(this.value));
+    public String toStr() {
+            if (this.value == 0) {
+                return String.format("%s", String.valueOf(this.type_) + String.valueOf(this.value));
+            }
+            return String.format("%s", String.valueOf(this.type_));
         }
-        return String.format("%s", String.valueOf(this.token));
     }
-}
+
