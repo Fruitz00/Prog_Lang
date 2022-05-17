c4fc4f662e79ce52503c005d7150b1932ca473f3
Ella Fritz Sarmiento <sellafritz@gmail.com>
2022-05-17 16:19:16 +0800

    [MOD] Change from char to string types
    Most changes in lexer paki check na lng

--- MP2/Lexer/Lexer.java
+++ MP2/Lexer/Lexer.java
@@ -9,16 +9,17 @@ public class Lexer {
     public Position pos;
     private Character current_char;
     private Position pos_start;
+    private Tokens tok = new Tokens();
     char charArr[];
     char tokens[];
     
-    public Lexer(String fn, String text){
+    Lexer(String fn, String text){
         //Convert String text to char array.
         charArr = text.toCharArray();
         this.fn = fn;
         this.text = text;
         this.pos = new Position(-1, 0, -1, fn, text); //Default from -1 
-        this.current_char = charArr[0]; //Pass value here. NullpointerException. Can be first elem of charArr
+        this.current_char = null;
         this.advance();
     }    
 
@@ -34,29 +35,27 @@ public class Lexer {
         // this.current_char = this.text[this.pos.idx] if this.pos.idx < len(this.text) else null;
     }
 
-
-
-    public List<Character> make_tokens(){
-        List<Character> token = new ArrayList<Character>();
+    public List<String> make_tokens(){
+        List<String> token = new ArrayList<String>();
         token.clear();
         try {
             while (this.current_char != null){
                 if(this.current_char == '\t'){
                     this.advance();
                 }else if(DIGITS.contains(Character.toString(this.current_char))){
-                    //If current char matches the digits array, tokens.append(this.makenumber)
+                    token.add(make_numbers());
                 }else if(this.current_char == '+'){
                     token.add(Tokens.TT_PLUS);
                 }else if(this.current_char == '-'){
                     token.add(Tokens.TT_MINUS);
                 }else if(this.current_char == '*'){
-                    
+                    token.add(Tokens.TT_MUL);
                 }else if(this.current_char == '/'){
-                    
+                    token.add(Tokens.TT_DIV);
                 }else if(this.current_char == '('){
-                    
+                    token.add(Tokens.TT_LPAREN);
                 }else if(this.current_char == ')'){
-                    
+                    token.add(Tokens.TT_RPAREN);
                 }else{
                     pos_start = pos.copy();
                     char char_ = this.current_char;
@@ -71,7 +70,26 @@ public class Lexer {
         return token;
     }
 
-    public void make_numbers(){
+    public String make_numbers(){
+        char num_str = ' ';
+        int dot_count = 0;
 
+        while(this.current_char != null && DIGITS.contains(Character.toString(this.current_char + '.'))){
+            if(this.current_char == '.'){
+                if(dot_count == 1) break;
+                dot_count += 1;
+                num_str += '.';
+            }
+            else{
+                num_str += this.current_char;
+                this.advance();
+            }
+        }
+
+        if(dot_count == 0){
+            return tok.toStr(Tokens.TT_INT, num_str);
+        }else{
+            return tok.toStr(Tokens.TT_FLOAT, num_str);
+        }
     }
 }
--- MP2/Lexer/Tokens.java
+++ MP2/Lexer/Tokens.java
@@ -5,26 +5,19 @@ public class Tokens {
     static final int[] DIGITS = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
     static final String TT_INT = "INT";
     static final String TT_FLOAT = "FLOAT";
-    static final char TT_PLUS = '+';
-    static final char TT_MINUS = '-';
+    static final String TT_PLUS = "PLUS";
+    static final String TT_MINUS = "MINUS";
+    static final String TT_MUL = "MUL";
     static final String TT_DIV = "DIV";
     static final String TT_LPAREN = "LPAREN";
     static final String TT_RPAREN = "RPAREN";
 
-    private String type_;
-    private int value;
 
-    public Tokens(String type_, int value){
-            this.type_ = type_;
-            this.value = value;
-    }
-
-
-    public String toStr() {
-            if (this.value == 0) {
-                return String.format("%s", String.valueOf(this.type_) + String.valueOf(this.value));
+    public String toStr(String type_, int value) {
+            if (value == 0) {
+                return String.format("%s", String.valueOf(type_) + String.valueOf(value));
             }
-            return String.format("%s", String.valueOf(this.type_));
+            return String.format("%s", String.valueOf(type_));
         }
     }
 
